5c96587e524325565739f83d72684a9edad1be10
Lucas Ramos <lucas.ramos.pessoa@hotmail.com>
2023-11-30 18:17:34 -0300

    Adding Fluent API Mapping, Creating Default Connection and Initial Migration.

--- CatalogoMinimalAPI/Context/AppDBContext.cs
+++ CatalogoMinimalAPI/Context/AppDBContext.cs
@@ -10,6 +10,38 @@ namespace CatalogoMinimalAPI.Context
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //Categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome)
+                .HasMaxLength(100)
+                .IsRequired();
+            mb.Entity<Categoria>().Property(c => c.Descricao)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            //Produto
+            mb.Entity<Produto>().HasKey(c => c.ProdutoId);
+            mb.Entity<Produto>().Property(c => c.Nome)
+                .HasMaxLength(100)
+                .IsRequired();
+            mb.Entity<Produto>().Property(c => c.Descricao)
+                .HasMaxLength(150)
+                .IsRequired();
+            mb.Entity<Produto>().Property(c => c.Imagem)
+                .HasMaxLength(100)
+                .IsRequired();
+            mb.Entity<Produto>().Property(c => c.Preco)
+                .HasPrecision(14, 2);
+
+
+            //Relacionamento
+
+            mb.Entity<Produto>()
+                .HasOne<Categoria>(c => c.Categoria)
+                .WithMany(p => p.Produtos)
+                .HasForeignKey(c => c.CategoriaId);
+        }
     }
 }
