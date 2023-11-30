53469d704cf3cfc2bb9d4ca42a373e2e926cb375
Lucas Ramos <lucas.ramos.pessoa@hotmail.com>
2023-11-30 16:45:29 -0300

    Creating Project, Adding gitIgnore Files, Adding Packages, Creating Models and DBContext

---
+++ CatalogoMinimalAPI/Context/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CatalogoMinimalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoMinimalAPI.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        
+    }
+}
